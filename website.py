267391b8a40e5ff355243ffe0519597424ab8837
unknown <vedasrikoonreddy@gmail.com>
2023-01-20 11:55:05 +0530

    my portfolio website

---
+++ website.py
@@ -0,0 +1,40 @@
+from flask import Flask, render_template, url_for, request, redirect
+import csv
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/<string:page_name>')
+def html_page(page_name):
+    return render_template(page_name)
+#get means the browser wants us to send informtion [post] browser wants us to save information
+
+#def write_to_file(data):
+#    with open('database.txt', mode='a') as database:
+#        email = data['email']
+#        subject = data['subject']
+#        message = data['message']
+#        file = database.write(f'\n{email},{subject},{message}')
+
+def write_to_csv(data):
+    with open('database.csv', newline='', mode='a') as database2:
+        email = data['email']
+        fname = data['fname']
+        lname = data['lname']
+        phone = data['phone']
+        places = data['places']
+        message = data['message']
+        csv_writer = csv.writer(database2,delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
+        csv_writer.writerow([email,fname,lname,phone,places,message])
+
+@app.route('/submit_form',methods=['POST','GET'])
+def submit_form():
+    if request.method == 'POST':
+     data = request.form.to_dict()
+     write_to_csv(data)
+     return redirect('/thankyou.html')
+    else:
+      return 'something went wrong, try again'
